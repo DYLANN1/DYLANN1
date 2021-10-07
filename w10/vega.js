9989926443a064d5455e92d3f53b0986b717656f
DYLANN1 <87954847+DYLANN1@users.noreply.github.com>
2021-10-07 14:06:26 +1100

    Delete w10 directory

--- w10/vega.js
+++
@@ -1,8 +0,0 @@
-var vg_1 = 'map.vega';
-vegaEmbed("#map", vg_1).then(function(result){
-    //Access Vega view instance
-}).catch(console.error)
-var vg_1 = 'slider.vega';
-vegaEmbed("#slider", vg_1).then(function(result){
-    //Access Vega view instance
-}).catch(console.error)
