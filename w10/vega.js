3184c0a8ab561fcb440ff0e350b9d7b8913aa29b
DYLANN1 <87954847+DYLANN1@users.noreply.github.com>
2021-10-07 14:04:59 +1100

    Add files via upload

---
+++ w10/vega.js
@@ -0,0 +1,8 @@
+var vg_1 = 'map.vega';
+vegaEmbed("#map", vg_1).then(function(result){
+    //Access Vega view instance
+}).catch(console.error)
+var vg_1 = 'slider.vega';
+vegaEmbed("#slider", vg_1).then(function(result){
+    //Access Vega view instance
+}).catch(console.error)
