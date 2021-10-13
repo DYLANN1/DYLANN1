5f8d440ded5a0657fc48c5813d9479f5c51dc5dc
DYLANN1 <87954847+DYLANN1@users.noreply.github.com>
2021-10-13 12:14:10 +1100

    Delete clean.py

--- clean.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-dataset = pd.read_csv('C:/Users/DYLAN/Downloads/FIT3179 asgn2/UFO.csv', encoding='cp1252')
-
-dataset = dataset.drop('X.1', axis=1)
-dataset = dataset.drop('X', axis=1)
-dataset = dataset[dataset.Country != 'CANADA']
-
-dataset.to_csv('C:/Users/DYLAN/Downloads/FIT3179 asgn2/UFO.csv')
